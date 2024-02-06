e0a2e2550277fddc157a4ba7a664517074326147
David Kerr <kerrky@gmail.com>
2024-02-06 14:55:38 -0500

    Complete Test

--- src/index.js
+++ src/index.js
@@ -1,22 +1,81 @@
 // index.js
-
+const ramenMenuDiv = document.getElementById('ramen-menu')
+let currentlyDisplayedRamen;
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  
+  const ramenDetailDiv = document.getElementById('ramen-detail')
+  const ramenImages = document.querySelector('img.detail-image')
+        ramenImages.src = ramen.image
+
+  const detailsComment = document.getElementById('comment-display')
+        detailsComment.innerText = ramen.comment
+
+  const detailsRating = document.getElementById('rating-display')
+        detailsRating.innerText = ramen.rating
+
+  const ramenName = document.querySelector('h2.name')
+        ramenName.innerText = ramen.name
+
+  const ramenRestaurant = document.querySelector('h3.restaurant')
+        ramenRestaurant.innerText = ramen.restaurant
+
+        
+    
 };
 
 const addSubmitListener = () => {
-  // Add code
+  const newRamenForm = document.getElementById('new-ramen')
+  newRamenForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newRamenName = document.getElementById('new-name')
+    const newRamenRestaurant = document.getElementById('new-restaurant')
+    const newRamenImage = document.getElementById('new-image')
+    const newRamenRating = document.getElementById('new-rating')
+    const newRamenComment = document.getElementById('new-comment')
+    const ramenImageElement = document.createElement('img')
+          ramenImageElement.src = newRamenImage.value
+        ramenMenuDiv.appendChild(ramenImageElement)
+    const newRamenObject = {
+            name: newRamenName.value,
+            restaurant: newRamenRestaurant.value,
+            image: newRamenImage.value,
+            rating: newRamenRating.value,
+            comment: newRamenComment.value
+                    }
+          ramenImageElement.addEventListener('click', () => {
+            handleClick(newRamenObject)
+          }) 
+    })
 }
 
 const displayRamens = () => {
-  // Add code
+  
+  
+  fetch('http://localhost:3000/ramens')
+  .then(response => response.json())
+  .then(ramenData => {
+        handleClick(ramenData[0])
+    ramenData.forEach(ramen => {
+      const imgElement = document.createElement('img')
+      imgElement.src = ramen.image
+      
+      ramenMenuDiv.appendChild(imgElement)
+      
+
+      imgElement.addEventListener('click', () => {
+        handleClick(ramen)
+      })
+      
+    });
+  })
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  displayRamens()
+  addSubmitListener()
 }
+
 
 main()
 
@@ -27,3 +86,5 @@ export {
   handleClick,
   main,
 };
+
+
